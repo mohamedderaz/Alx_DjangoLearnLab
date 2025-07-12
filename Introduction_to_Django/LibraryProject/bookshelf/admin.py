dae453e5cabf8852d954fc8aeb0192e178561841
mohamedderaz <deraz.mohamed11@gmail.com>
2025-07-12 19:00:47 +0300

    Add files via upload

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')   # الأعمدة اللي هتظهر في القائمة
+    search_fields = ('title', 'author')                      # إمكانيات البحث
+    list_filter = ('publication_year',)                      # فلاتر جانبية
 
 # Register your models here.
