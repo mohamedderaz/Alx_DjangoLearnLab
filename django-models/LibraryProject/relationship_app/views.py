fb84db4d1d8a5eb306838b24fbe5ccf0070fc3a7
mohamedderaz <deraz.mohamed11@gmail.com>
2025-07-18 23:56:16 +0300

    Add files via upload

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,19 +1,16 @@
 from django.shortcuts import render
-from .models import Book
-
+from .models import Book, Library
 from django.views.generic.detail import DetailView
-from .models import Library
 
-
+# ✅ Function-based View
 def list_books(request):
-    books = Book.objects.select_related('author').all()
-    return render(request, 'list_books.html', {'books': books})
+    books = Book.objects.all()  # <-- مطلوب حسب الاختبار
+    return render(request, 'relationship_app/list_books.html', {'books': books})  # <-- المسار الصحيح
 
+# ✅ Class-based View
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'  # مسار صحيح للقالب
     context_object_name = 'library'
-
-
 
 # Create your views here.
