8a3bbedd8366b12fc414a7c9b266fffa8afc1664
mohamedderaz <deraz.mohamed11@gmail.com>
2025-07-19 00:22:12 +0300

    Add files via upload

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,8 @@ from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth import logout
 
 # ✅ Function-based View
 def list_books(request):
