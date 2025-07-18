fe1715706dad6e71c062468fa503593f0ca23c18
mohamedderaz <deraz.mohamed11@gmail.com>
2025-07-19 02:49:27 +0300

    Add files via upload

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path,include
 from .views import list_books, LibraryDetailView
+from django.contrib.auth import views as auth_views
+from . import views
+from .views import admin_view, librarian_view, member_view
 
 urlpatterns = [
     path('books/', list_books, name='book-list'),
@@ -7,4 +10,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
+    path('admin-role/', admin_view, name='admin_view'),
+    path('librarian-role/', librarian_view, name='librarian_view'),
+    path('member-role/', member_view, name='member_view'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,8 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import user_passes_test, login_required
+from .models import UserProfile
 
 # ✅ Function-based View
 def list_books(request):
@@ -16,5 +18,23 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'  # مسار صحيح للقالب
     context_object_name = 'library'
+    def check_role(role):
+    def has_role(user):
+        return hasattr(user, 'userprofile') and user.userprofile.role == role
+
+@login_required
+@check_role('Admin')
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@login_required
+@check_role('Librarian')
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@login_required
+@check_role('Member')
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
 
 # Create your views here.
