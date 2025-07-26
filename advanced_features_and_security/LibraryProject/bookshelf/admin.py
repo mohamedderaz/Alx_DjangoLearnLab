4cf54c9a62c70416fbf2e05f5bd6bd34d4609578
mohamedderaz <deraz.mohamed11@gmail.com>
2025-07-26 12:51:37 +0300

    Add files via upload

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    # لو عندك خصائص إضافية ممكن تضيفها هنا
+    # مثلا:
+    # list_display = ['username', 'email', 'role', 'is_staff']
+    # fieldsets = UserAdmin.fieldsets + (
+    #     (None, {'fields': ('role',)}),
+    # )
+
+admin.site.register(CustomUser, CustomUserAdmin)
 
 # Register your models here.
