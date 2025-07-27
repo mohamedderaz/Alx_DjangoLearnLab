b32d030a1ae04f52c6d9d525feaa70dc0bcd327d
mohamedderaz <deraz.mohamed11@gmail.com>
2025-07-27 12:15:15 +0300

    Add files via upload

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,34 +1,34 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Book
+from .models import book
 
 @permission_required('relationship_app.can_view', raise_exception=True)
-def Book_list(request):
-    Books = Book.objects.all()
-    return render(request, 'relationship_app/Book_list.html', {'Books': Books})
+def book_list(request):
+    books = book.objects.all()
+    return render(request, 'relationship_app/book_list.html', {'books': books})
 
 
 @permission_required('relationship_app.can_create', raise_exception=True)
-def create_Book(request):
+def create_book(request):
     if request.method == 'POST':
-        # logic to create Book
+        # logic to create book
         ...
-    return render(request, 'relationship_app/create_Book.html')
+    return render(request, 'relationship_app/create_book.html')
 
 
 @permission_required('relationship_app.can_edit', raise_exception=True)
-def edit_Book(request, Book_id):
-    Book = get_object_or_404(Book, id=Book_id)
+def edit_book(request, book_id):
+    book = get_object_or_404(book, id=book_id)
     if request.method == 'POST':
-        # logic to update Book
+        # logic to update book
         ...
-    return render(request, 'relationship_app/edit_Book.html', {'Book': Book})
+    return render(request, 'relationship_app/edit_book.html', {'book': book})
 
 
 @permission_required('relationship_app.can_delete', raise_exception=True)
-def delete_Book(request, Book_id):
-    Book = get_object_or_404(Book, id=Book_id)
-    Book.delete()
-    return redirect('Book_list')
+def delete_book(request, book_id):
+    book = get_object_or_404(book, id=book_id)
+    book.delete()
+    return redirect('book_list')
 # Create your views here.
