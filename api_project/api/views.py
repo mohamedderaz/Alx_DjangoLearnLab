01c623223c3a149e84b035e5a82e8e6d1ff8eaef
mohamedderaz <deraz.mohamed11@gmail.com>
2025-07-29 15:39:42 +0300

    Add files via upload

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,19 +1,13 @@
-from django.shortcuts import render
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import Book
 from .serializers import BookSerializer
+
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-
-    # Existing ListAPIView
-class BookList(generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
-# New ViewSet for full CRUD
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-# Create your views here.
+    permission_classes = [IsAuthenticated]  # لازم يكون المستخدم مسجل دخول
