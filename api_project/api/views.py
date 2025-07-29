d0dc99179a84911bcc07591e3a55d61576861855
mohamedderaz <deraz.mohamed11@gmail.com>
2025-07-29 15:35:09 +0300

    Add files via upload

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+    # Existing ListAPIView
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# New ViewSet for full CRUD
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 # Create your views here.
