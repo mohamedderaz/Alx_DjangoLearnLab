adbc8c0d9134f5e19ca822ae3c685c19c5ad7aa8
mohamedderaz <deraz.mohamed11@gmail.com>
2025-07-29 15:40:33 +0300

    Add files via upload

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 from .views import BookList, BookViewSet
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
-    # Route for read-only list view
     path('books/', BookList.as_view(), name='book-list'),
-
-    # Include router URLs
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # الحصول على التوكن
     path('', include(router.urls)),
 ]
+  
