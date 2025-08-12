0d7969458bc6ed32be5296b0284911cfc8503860
mohamedderaz <deraz.mohamed11@gmail.com>
2025-08-12 22:18:43 +0300

    Add files via upload

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,23 @@
-from django.views.generic import ListView, DetailView, UpdateView, DeleteView
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
+
+# List + Create
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
+
+# Retrieve + Update + Delete
+class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    from django.views.generic import ListView, DetailView, UpdateView, DeleteView
 from .models import Book  # أو أي موديل عندك
 
 class BookListView(ListView):
