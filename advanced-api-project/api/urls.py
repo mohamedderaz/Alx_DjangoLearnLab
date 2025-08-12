38954df97c823465b44ea387390f15ed19d79676
mohamedderaz <deraz.mohamed11@gmail.com>
2025-08-12 22:19:55 +0300

    Add files via upload

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -9,10 +9,33 @@ router.register(r'books', BookViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 ]
-from django.urls import path
-from .views import BookListCreateView, BookRetrieveUpdateDestroyView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Book
 
-urlpatterns = [
-    path('books/', BookListCreateView.as_view(), name='book-list-create'),
-    path('books/<int:pk>/', BookRetrieveUpdateDestroyView.as_view(), name='book-detail'),
-]
+class BookListView(ListView):
+    model = Book
+    template_name = 'books/book_list.html'
+    context_object_name = 'books'
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'books/book_detail.html'
+    context_object_name = 'book'
+
+class BookCreateView(CreateView):
+    model = Book
+    fields = ['title', 'author', 'published_date']
+    template_name = 'books/book_form.html'
+    success_url = reverse_lazy('book-list')
+
+class BookUpdateView(UpdateView):
+    model = Book
+    fields = ['title', 'author', 'published_date']
+    template_name = 'books/book_form.html'
+    success_url = reverse_lazy('book-list')
+
+class BookDeleteView(DeleteView):
+    model = Book
+    template_name = 'books/book_confirm_delete.html'
+    success_url = reverse_lazy('book-list')
