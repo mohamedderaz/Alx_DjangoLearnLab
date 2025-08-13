c0266a8c19e2f59f74c34c8eca1d602e3a221e57
mohamedderaz <deraz.mohamed11@gmail.com>
2025-08-13 23:26:05 +0300

    Add files via upload

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,8 +4,8 @@ from .views import (
 )
 
 urlpatterns = [
-    path('login',  BlogLoginView.as_view(), name='login'),      # /login
-    path('logout', BlogLogoutView.as_view(), name='logout'),    # /logout
-    path('register', register, name='register'),                # /register
-    path('profile', profile, name='profile'),                  # /profile
+    path('login/',  BlogLoginView.as_view(), name='login'),      
+    path('logout/', BlogLogoutView.as_view(), name='logout'),    
+    path('register/', register, name='register'),                
+    path('profile/', profile, name='profile'),                  
 ]
