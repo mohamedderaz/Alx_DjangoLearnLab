b826c3a8c4812afdd16ac1307c6b6a4c8f987cd6
mohamedderaz <deraz.mohamed11@gmail.com>
2025-08-18 23:04:06 +0300

    Add files via upload

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -34,3 +34,17 @@ class CommentViewSet(viewsets.ModelViewSet):
         serializer.save(author=self.request.user)from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def feed(request):
+    # Posts من الناس اللي المستخدم بيتابعهم
+    following_users = request.user.following.all()
+    posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
