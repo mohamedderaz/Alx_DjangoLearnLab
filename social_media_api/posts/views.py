58e492147d6700c98b16070e889f6df5cc9c4c42
mohamedderaz <deraz.mohamed11@gmail.com>
2025-08-18 23:17:23 +0300

    Add files via upload

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,17 +1,28 @@
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
 from .models import Post, Like
+from notifications.models import Notification  # استدعاء الموديل
+from rest_framework.generics import get_object_or_404
+
 
 class LikePostView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, pk):
-        # نستخدم get_object_or_404 هنا
-        post = generics.get_object_or_404(Post, pk=pk)
+        post = get_object_or_404(Post, pk=pk)
         like, created = Like.objects.get_or_create(user=request.user, post=post)
         if not created:
             return Response({"detail": "You already liked this post."}, status=status.HTTP_400_BAD_REQUEST)
+
+        # 🔔 هنا نضيف Notification
+        if post.author != request.user:  # منطق بسيط: ما تبعتش إشعار لنفسك
+            Notification.objects.create(
+                recipient=post.author,
+                actor=request.user,
+                verb="liked",
+                target=post
+            )
+
         return Response({"detail": "Post liked successfully."}, status=status.HTTP_201_CREATED)
 
 
@@ -19,8 +30,7 @@ class UnlikePostView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, pk):
-        # برضه هنا
-        post = generics.get_object_or_404(Post, pk=pk)
+        post = get_object_or_404(Post, pk=pk)
         like = Like.objects.filter(user=request.user, post=post).first()
         if like:
             like.delete()
